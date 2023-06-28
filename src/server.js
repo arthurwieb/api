8867e59d49c26b552fd342cca9aad29add94adad
Arthur Machado Wiebusch <arthur.wieb@gmail.com>
2023-06-27 21:16:21 -0700

    wow

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/uploads");
+const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.UPLOAD_FOLDER));
 app.use(routes);
