5cc7c87dcba9d7945a1745f9584053f2551a5237
Arthur Machado Wiebusch <arthur.wieb@gmail.com>
2023-07-18 23:58:05 -0700

    ajustes finais

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -18,15 +18,6 @@ class NotesController {
       rating,
     });
 
-    // const linksInsert = links.map((link) => {
-    //   return {
-    //     note_id,
-    //     url: link,
-    //   };
-    // });
-
-    // await knex("links").insert(linksInsert);
-
     const tagsInsert = tags.map((name) => {
       return {
         note_id,
@@ -45,14 +36,10 @@ class NotesController {
 
     const note = await knex("notes").where({ id }).first();
     const tags = await knex("tags").where({ note_id: id }).orderBy("name");
-    // const links = await knex("links")
-    //   .where({ note_id: id })
-    //   .orderBy("created_at");
-    //     return response.json(note);
+
     return response.json({
       ...note,
       tags,
-      // links,
     });
   }
 
